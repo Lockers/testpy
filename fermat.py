377dec475f07ed97719bbea2c4d5ae891d01d347
Matt Locklin <mattlocklin@live.co.uk>
2020-02-08 11:07:31 +0000

    'finished_exercise_2_and_3'

---
+++ fermat.py
@@ -0,0 +1,14 @@
+def check_fermat(a, b, c, n):
+    if n > 2:
+        if a**n + b**n == c**n:
+            print('Holy smokes, Fermat was wrong!')
+        else:
+            print("No, that doesn't work")
+
+
+a = input('Give me your first number')
+b = input('Give me your second number')
+c = input('Give me your third number')
+n = input('Give me your n number?')
+
+check_fermat(int(a), int(b), int(c), int(n))
